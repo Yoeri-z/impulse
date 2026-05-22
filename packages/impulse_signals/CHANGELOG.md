19cdac17433754271e5c6498689d7b1ce775a994
Yoeri <yoeriz123@gmail.com>
2026-05-22 14:34:11 +0200

    fixed incorrect export and pushed to pub

--- packages/impulse_signals/CHANGELOG.md
+++ packages/impulse_signals/CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.1.2
+
+- `impulse_signals` accidentally exported itsself instead of `impulse_flutter` (oops)
+  this is now fixed.
+
 ## 0.1.1
 
 - Minor readme improvements
--- packages/impulse_signals/lib/impulse_signals.dart
+++ packages/impulse_signals/lib/impulse_signals.dart
@@ -1,7 +1,7 @@
 /// Signals integration for the Impulse state management library.
 library;
 
-export 'package:impulse_signals/impulse_signals.dart';
+export 'package:impulse_flutter/impulse_flutter.dart';
 export 'package:signals_flutter/signals_flutter.dart';
 export 'src/controller.dart';
 export 'src/attempt.dart';
